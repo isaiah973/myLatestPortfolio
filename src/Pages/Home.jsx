7c52dd8b88fe3f518c3bac61e1a3bf9b67e0b7c0
isaiah973 <adeolaisaiah01@gmail.com>
2025-12-10 14:14:47 +0100

    added footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "../Components/HeroSection";
 import React from "react";
 import Portfolio from "../Components/Portfolio";
+import Footer from "../Components/Footer";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Portfolio />
+      <Footer />
     </>
   );
 }
